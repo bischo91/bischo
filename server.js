78b2f5ada88c309e8250a358431209cddcb3e567
bischo91 <bischo91@gmail.com>
2021-12-16 19:52:50 -0500

    contact us email test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
@@ -18,59 +18,59 @@ app.listen(port);
 
 // ContactForm
 
-// var express = require('express');
-// var router = express.Router();
-// var nodemailer = require('nodemailer');
-// var cors = require('cors');
-// const creds = require('./config');
-//
-// var transport = {
-//     host: 'smtp.example.com', // Don’t forget to replace with the SMTP host of your provider
-//     port: 587,
-//     auth: {
-//     user: creds.USER,
-//     pass: creds.PASS
-//   }
-// }
-//
-// var transporter = nodemailer.createTransport(transport)
-//
-// transporter.verify((error, success) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Server is ready to take messages');
-//   }
-// });
-//
-// router.post('/send', (req, res, next) => {
-//   var name = req.body.name
-//   var email = req.body.email
-//   var message = req.body.message
-//   var content = `name: ${name} \n email: ${email} \n message: ${message} `
-//
-//   var mail = {
-//     from: name,
-//     to: 'RECEIVING_EMAIL_ADDRESS_GOES_HERE',  // Change to email address that you want to receive messages on
-//     subject: 'New Message from Contact Form',
-//     text: content
-//   }
-//
-//   transporter.sendMail(mail, (err, data) => {
-//     if (err) {
-//       res.json({
-//         status: 'fail'
-//       })
-//     } else {
-//       res.json({
-//        status: 'success'
-//       })
-//     }
-//   })
-// })
-//
-// const app = express()
-// app.use(cors())
-// app.use(express.json())
-// app.use('/', router)
-// app.listen(3002)
+var express = require('express');
+var router = express.Router();
+var nodemailer = require('nodemailer');
+var cors = require('cors');
+const creds = require('./config');
+
+var transport = {
+    host: 'smtp.example.com', // Don’t forget to replace with the SMTP host of your provider
+    port: 587,
+    auth: {
+    user: creds.USER,
+    pass: creds.PASS
+  }
+}
+
+var transporter = nodemailer.createTransport(transport)
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take messages');
+  }
+});
+
+router.post('/send', (req, res, next) => {
+  var name = req.body.name
+  var email = req.body.email
+  var message = req.body.message
+  var content = `name: ${name} \n email: ${email} \n message: ${message} `
+
+  var mail = {
+    from: name,
+    to: 'RECEIVING_EMAIL_ADDRESS_GOES_HERE',  // Change to email address that you want to receive messages on
+    subject: 'New Message from Contact Form',
+    text: content
+  }
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: 'fail'
+      })
+    } else {
+      res.json({
+       status: 'success'
+      })
+    }
+  })
+})
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/', router)
+app.listen(3002)
