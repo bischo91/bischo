8c702743c7becece1e3462e508fb15d5e2d97e98
bischo91 <bischo91@gmail.com>
2021-12-18 16:18:40 -0500

    pdf was a problem?

--- src/routes/Resume.js
+++ src/routes/Resume.js
@@ -1,6 +1,7 @@
 import React from "react";
 import resume from "../images/resume.jpg";
-import resume_pdf from "../pdf/resume.pdf";
+// import resume_pdf from "../pdf/resume.pdf";
+// href={resume_pdf} 
 
 class Resume extends React.Component {
   componentDidMount() {}
@@ -10,7 +11,7 @@ class Resume extends React.Component {
       <div className="w-5/6 h-full mt-24 m-auto bg-primary min-h-screen text-primary">
         <h1 className="text-4xl mb-8">RESUME</h1>
         <div className="row space-y-4 mt-8">
-          <a href={resume_pdf} target="_blank" rel="noopener noreferrer">
+          <a target="_blank" rel="noopener noreferrer">
             <img
               src={resume}
               alt="Resume"
