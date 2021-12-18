8652168c0697643187812e81342907c7a94744fb
bischo91 <bischo91@gmail.com>
2021-12-17 23:27:31 -0500

    heroku fix

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT||'23054';
+const port = process.env.PORT || '8080';
 const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
@@ -13,4 +13,5 @@ app.get('/ping', function (req, res) {
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+app.set("port", port)
 app.listen(port);
