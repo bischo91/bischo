7b59d80c7163f480aaade9c3157efd00d7575e79
Brian Cho <brian.cho@clearobject.com>
2025-08-25 14:48:39 +0900

    updated: resume link

--- src/routes/Resume.js
+++ src/routes/Resume.js
@@ -9,7 +9,8 @@ class Resume extends React.Component {
         <h1 className="px-2 mb-8 font-mono text-4xl">RESUME</h1>
         <div className="space-y-4 sm:-ml-4 row">
           <iframe
-            src="https://drive.google.com/file/d/1QW0Az1AvOdzulRqyY0XDTCKrFNTMl__9/preview"
+            //src="https://drive.google.com/file/d/1QW0Az1AvOdzulRqyY0XDTCKrFNTMl__9/preview"
+            src="https://drive.google.com/file/d/126WzPb31tmKBxZQhxpsKpio-drWcsEvI/view?usp=sharing"
             className="w-[90vw] aspect-[2/3] sm:w-[80vw] md:w-[70vw] lg:w-[60vw] xl:w-[40vw] max-h-screen"
             title="resume"
           />
