85fc4a6a3c18e4822c2b1f711891f8c911e1c886
Brian Cho <brian.cho@clearobject.com>
2026-01-04 15:15:10 +0900

    added: analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.ico';
+	import Analytics from '$lib/components/analytics.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<Analytics />
 {@render children()}
